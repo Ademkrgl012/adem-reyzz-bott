ea716a5f081a4537529c785d1cff131aa6a991cd
Glitch (discordjsv12basitaltyapi) <none>
2020-10-14 13:00:46 +0000

    🌷🏥 Checkpoint ./komutlar/kullanıcı.js:22384402/1242 ./komutlar/moderasyon.js:22384402/804

--- komutlar/kullanıcı.js
+++ komutlar/kullanıcı.js
@@ -5,11 +5,11 @@ const db = require("quick.db");
 exports.run = async (client, message, args) => {
 
 const kullanıcı = new Discord.MessageEmbed()
-.setColor("RANDOM").setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny Kullanıcı Komutları <a:Yldz:742698148329291826>")
+.setColor("RANDOM")
+.setTitle("<a:yldz:765522787610787850> Dwonzy Bot Kullanıcı Komutları! <a:yldz:765522787610787850>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **-avatar** = Avatarınıza bakarsınız. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-yetkilerim** = Yetkilerini görürsün. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-profil** = Profilini görürsün. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-sunucuresmi** = Sunucu resmini gösterir. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-ping** = Botun Pingine Bakarsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-id** = Birisinin id'sine Bakarsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-davet** = Beni Sunucuna Ekle. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-botbilgi** = Bot istatistiklerini görürsünüz. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. <a:developeremoji:749525084586115153>")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:yan:765915647557107743> **-avatar** = Avatarınıza bakarsınız.  \n <a:yan:765915647557107743> **-yetkilerim** = Yetkilerini görürsün.  \n <a:yan:765915647557107743> **-profil** = Profilini görürsün.  \n <a:yan:765915647557107743> **-sunucuresmi** = Sunucu resmini gösterir.  \n <a:yan:765915647557107743> **-ping** = Botun Pingine Bakarsın.  \n <a:yan:765915647557107743> **-id** = Birisinin id'sine Bakarsın.  \n <a:yan:765915647557107743> **-davet** = Beni Sunucuna Ekle.  \n <a:yan:765915647557107743> **-botbilgi** = Bot istatistiklerini görürsünüz.  \n <a:yan:765915647557107743> **-bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. ")
+.setImage("https://cdn.discordapp.com/attachments/765269752788025375/765912834592342016/Untitled_design_1.gif")
 message.channel.send(kullanıcı)
 }
 
@@ -24,5 +24,5 @@ exports.help = {
   name: 'kullanıcı',
   category: 'kullanıcı',
   description: 'Yardım Menüsü.',
-   usage:'!kullanıcı'
+   usage:'-kullanıcı'
 }
