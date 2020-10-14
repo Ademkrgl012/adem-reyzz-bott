69f034ef69ca180de935d21a5c39ada8ad8a996b
Glitch (discordjsv12basitaltyapi) <none>
2020-10-14 13:11:56 +0000

    📉🍦 Checkpoint ./komutlar/moderasyon.js:22384402/770

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -6,11 +6,10 @@ exports.run = async (client, message, args) => {
 
 const mod = new Discord.MessageEmbed()
 .setColor("RANDOM")
-.setAuthor("Destiny v12 sürümüyle sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny Moderasyon Komutları <a:Yldz:742698148329291826>")
+.setTitle("<a:yldz:765522787610787850> Dwonzy Bot Moderasyon Komutları! <a:yldz:765522787610787850>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **-sil** = Yazdığınız miktarda mesajı siler. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-ban** = Etiketlediğiniz kişiyi banlarsınız. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-kick** = Etiketlediğiniz kişiyi atarsınız. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-duyuru** = Bota duyuru yaptırırsınız.  \n <a:yan:765915647557107743> **-küfür** = Küfür engel sistemini açarsınız.  \n <a:yan:765915647557107743> **-reklam** = Reklam engel sistemi açarsınız.  \n <a:yan:765915647557107743> **-slowmode** = Yavaş modu ayarlarsınız.  \n <a:yan:765915647557107743> **-forceban** = Birisine id ban atarsınız.  \n <a:yan:765915647557107743> **-unban** = Birisinin banını açarsınız.  \n <a:yan:765915647557107743> **-sa-as** = Bot biri sa dedimi cevap verir.  \n <a:yan:765915647557107743> **-sunucubilgi** = Sunucu bilgilerini görürsün.  \n <a:yan:765915647557107743> **-üyedurum** = Üyelerin durumlarını görürsün.  \n <a:yan:765915647557107743> **-çekiliş** = Çekiliş başlatırsınız. ")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:yan:765915647557107743> **-sil** = Yazdığınız miktarda mesajı siler.  \n <a:yan:765915647557107743> **-ban** = Etiketlediğiniz kişiyi banlarsınız.  \n <a:yan:765915647557107743> **-kick** = Etiketlediğiniz kişiyi atarsınız.  \n <a:yan:765915647557107743> **-duyuru** = Bota duyuru yaptırırsınız.  \n <a:yan:765915647557107743> **-küfür** = Küfür engel sistemini açarsınız.  \n <a:yan:765915647557107743> **-reklam** = Reklam engel sistemi açarsınız.  \n <a:yan:765915647557107743> **-slowmode** = Yavaş modu ayarlarsınız.  \n <a:yan:765915647557107743> **-forceban** = Birisine id ban atarsınız.  \n <a:yan:765915647557107743> **-unban** = Birisinin banını açarsınız.  \n <a:yan:765915647557107743> **-sa-as** = Bot biri sa dedimi cevap verir.  \n <a:yan:765915647557107743> **-sunucubilgi** = Sunucu bilgilerini görürsün.  \n <a:yan:765915647557107743> **-üyedurum** = Üyelerin durumlarını görürsün.  \n <a:yan:765915647557107743> **-çekiliş** = Çekiliş başlatırsınız. ")
+.setImage("https://cdn.discordapp.com/attachments/765269752788025375/765912834592342016/Untitled_design_1.gif")
 message.channel.send(mod)
 }
 
@@ -25,5 +24,5 @@ exports.help = {
   name: 'moderasyon',
   category: 'kullanıcı',
   description: 'Yardım Menüsü.',
-   usage:'!moderasyon'
+   usage:'-moderasyon'
 }
