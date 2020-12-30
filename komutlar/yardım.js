8b93a4c33a5adc810cc2478c1d1ffe3017e76e7f
Glitch (discordjsv12basitaltyapi) <none>
2020-12-30 15:25:13 +0000

    🏙🐯 Checkpoint ./ayarlar.json:22384402/159 ./komutlar/yardım.js:22384402/188

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,10 +6,9 @@ exports.run = async (client, message, args) => {
 
 const yardım = new Discord.MessageEmbed()
 .setColor("BLACK")
-.setAuthor("Starby.")
-.setTitle("<a:yldz:765522787610787850> Starby Yardım Menüsüne Hoşgeldin! <a:yldz:765522787610787850>")
+.setTitle("<a:yr_selam:779001625414074448> Yrnex Yardım Menüsüne Hoşgeldin!")
  .setTimestamp()
-.setDescription("<a:panda2:765633198440841246> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlar:765523861806252064> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:ur_hopladc:765536907315773450> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:sarey:765533152248594484> **-logo** = __Logo komutlarını görüntülersiniz.__")
+.setDescription("<a:kitab:775385879618519062> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlar:765523861806252064> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:ur_hopladc:765536907315773450> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:sarey:765533152248594484> **-logo** = __Logo komutlarını görüntülersiniz.__")
 .setImage("https://cdn.discordapp.com/attachments/765269752788025375/765912834592342016/Untitled_design_1.gif")
 message.channel.send(yardım)
 }
