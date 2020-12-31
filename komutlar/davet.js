0676ba57d59b06afd7ab93c3e7badc47d17dba73
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 14:57:09 +0000

    🎎🐝 Checkpoint ./komutlar/davet.js:22384402/1

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   const davet = new Discord.MessageEmbed()
   .setColor("RANDOM")
   .setTitle("İşte Davet Linklerim <a:seksiemojiymishih:788680445230972950>")
-  .setDescription("[__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot) \n [__**Destek Sunucusu**__](ttps://discord.gg/RZheu3F2bM)
+  .setDescription("[__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot) \n [__**Destek Sunucusu**__](ttps://discord.gg/RZheu3F2bM")
   message.channel.send(davet)
 }
 
