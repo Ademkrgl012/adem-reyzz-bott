ccfdede7a09c92128a638583c5d093807ce5018d
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 08:42:55 +0000

    📰🐡 Checkpoint ./komutlar/yardım.js:22384402/4064

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,27 +1,29 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 
-
 exports.run = async (client, message, args) => {
-
-const yardım = new Discord.MessageEmbed()
-.setColor("BLACK")
-.setTitle("<a:kitab:775385879618519062> Yrnex Yardım Menüsüne Hoşgeldin!")
- .setTimestamp()
-.setDescription("🔰 **y!moderasyon** = __Moderasyon komutlarını görüntülersiniz.__\n 👥 **y!kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__\n <:YouTube:791401370560495668> **y!abone-sistemi** = __Abone Rol komutlarını görüntülersiniz.__")
- .setImage('https://cdn.discordapp.com/attachments/781872196654071819/793873257395912704/standard_19.gif')
-message.channel.send(yardım)
-}
+  const yardım = new Discord.MessageEmbed()
+    .setColor("BLACK")
+    .setTitle("<a:kitab:775385879618519062> Yrnex Yardım Menüsüne Hoşgeldin!")
+    .setTimestamp()
+    .setDescription(
+      "🔰 **y!moderasyon** = __Moderasyon komutlarını görüntülersiniz.__\n 👥 **y!kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__\n <:YouTube:791401370560495668> **y!abone-sistemi** = __Abone Rol komutlarını görüntülersiniz.__"
+    )
+    .setImage(
+      "https://cdn.discordapp.com/attachments/781872196654071819/793873257395912704/standard_19.gif"
+    );
+  message.channel.send(yardım);
+};
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-   aliases: ["help","y"],
-  permLevel: `Yetki gerekmiyor.` 
+  enabled: true,
+  guildOnly: false,
+  aliases: ["help", "y"],
+  permLevel: `Yetki gerekmiyor.`
 };
 
 exports.help = {
-  name: 'yardım',
-  category: 'kullanıcı',
-  description: 'Yardım Menüsü.',
-   usage:'y!yardım'
-}
+  name: "yardım",
+  category: "kullanıcı",
+  description: "Yardım Menüsü.",
+  usage: "y!yardım"
+};
