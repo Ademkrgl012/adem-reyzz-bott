813d22370e3b7d22d97faf6f7df3edbeea98f2a7
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 06:57:40 +0000

    👟🐁 Checkpoint ./komutlar/kullanıcı.js:22384402/5974

--- komutlar/kullanıcı.js
+++ komutlar/kullanıcı.js
@@ -1,28 +1,30 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 
-
 exports.run = async (client, message, args) => {
-
-const kullanıcı = new Discord.MessageEmbed()
-.setColor("RANDOM")
-.setTitle("👥 Yrnex Kullanıcı Komutları!")
- .setTimestamp()
-.setDescription("<a:yan:765915647557107743> **y!avatar** = Avatarınıza bakarsınız.  \n <a:yan:765915647557107743> **y!yetkilerim** = Yetkilerini görürsün.  \n <a:yan:765915647557107743> **y!profil** = Profilini görürsün.  \n <a:yan:765915647557107743> **y!sunucuresmi** = Sunucu resmini gösterir.  \n <a:yan:765915647557107743> **y!ping** = Botun Pingine Bakarsın.  \n <a:yan:765915647557107743> **y!id** = Birisinin id'sine Bakarsın.  \n <a:yan:765915647557107743> **y!davet** = Beni Sunucuna Ekle.  \n <a:yan:765915647557107743> **y!botbilgi** = Bot istatistiklerini görürsünüz.  \n <a:yan:765915647557107743> **y!bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. ")
-.setImage("https://cdn.discordapp.com/attachments/765269752788025375/765912834592342016/Untitled_design_1.gif")
-message.channel.send(kullanıcı)
-}
+  const kullanıcı = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("👥 Yrnex Kullanıcı Komutları!")
+    .setTimestamp()
+    .setDescription(
+      "<:emoji_3:793928384932610099>  **y!avatar** = Avatarınıza bakarsınız.  \n <:emoji_3:793928384932610099> **y!yetkilerim** = Yetkilerini görürsün.  \n <:emoji_3:793928384932610099> **y!profil** = Profilini görürsün.  \n <:emoji_3:793928384932610099> **y!sunucuresmi** = Sunucu resmini gösterir.  \n <:emoji_3:793928384932610099> **y!ping** = Botun Pingine Bakarsın.  \n <:emoji_3:793928384932610099> **y!id** = Birisinin id'sine Bakarsın.  \n <:emoji_3:793928384932610099> **y!davet** = Beni Sunucuna Ekle.  \n <:emoji_3:793928384932610099> **y!istatistik** = Bot istatistiklerini görürsünüz.  \n <:emoji_3:793928384932610099> **y!bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. "
+    )
+    .setImage(
+      "https://cdn.discordapp.com/attachments/765269752788025375/765912834592342016/Untitled_design_1.gif"
+    );
+  message.channel.send(kullanıcı);
+};
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-   aliases: [],
-  permLevel: `Yetki gerekmiyor.` 
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: `Yetki gerekmiyor.`
 };
 
 exports.help = {
-  name: 'kullanıcı',
-  category: 'kullanıcı',
-  description: 'Yardım Menüsü.',
-   usage:'y!kullanıcı'
-}
+  name: "kullanıcı",
+  category: "kullanıcı",
+  description: "Yardım Menüsü.",
+  usage: "y!kullanıcı"
+};
