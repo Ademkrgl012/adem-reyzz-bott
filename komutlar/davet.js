e6fdea3880c0db4911d0afaf5f06d3b98d8092f9
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 16:23:10 +0000

    🍳📅 Checkpoint ./komutlar/davet.js:22384402/1463

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,14 +1,14 @@
-const  Discord = require("discord.js"); 
+const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-
   const davet = new Discord.MessageEmbed()
-  .setColor("RANDOM")
-  .setTitle("İşte Davet Linklerim <a:yrnex_hypes:794222389584068618>")
-  .setDescription("<a:yrnex_partner:794222684233138197>  [__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot) \n <a:yrnex_partner:794222684233138197>  [__**Destek Sunucum**__](https://discord.gg/RZheu3F2bM) \n <a:yrnex_partner:794222684233138197>  [__**Web Sitem**__](https://yrnex-bot-.glitch.me)")
-  message.channel.send(davet)
-}
-
+    .setColor("RANDOM")
+    .setTitle("İşte Davet Linklerim <a:yrnex_hypes:794222389584068618>")
+    .setDescription(
+      "<a:yrnex_partner:794222684233138197>  [__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot) \n <a:yrnex_partner:794222684233138197>  [__**Destek Sunucum**__](https://discord.gg/RZheu3F2bM)"
+    );
+  message.channel.send(davet);
+};
 
 exports.conf = {
   enabled: true,
@@ -18,7 +18,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'davet',
-  description: '',
-  usage: ''
+  name: "davet",
+  description: "",
+  usage: ""
 };
