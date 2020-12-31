fc853b3ac65617d0e78e33c263d84d8cd64aecc3
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 07:44:44 +0000

    🙅💡 Checkpoint ./komutlar/sil.js:22384402/6276

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -1,37 +1,43 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
+const data = require("quick.db");
 
-exports.run = function(client, message, args) {
-  
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`Bu komutu kullanabilmek için **Mesajları Yönet** iznine sahip olmalısın!`);
-  
-if(isNaN(args[0])) {
-  var errembed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .addField(`Yanlış Kullanım!`, `Bir rakam yazmalısın!`)
-    .addField(`Doğru Kullanım:`, `${ayarlar.prefix}sil <temizlenecek mesaj sayısı>`)
-return message.channel.send(errembed);
-}
-  
-if (args[0] < 1) return message.reply("**1** adetten az mesaj silemem!")
-if (args[0] > 100) return message.reply("**100** adetten fazla mesaj silemem!")
-  
-message.channel.bulkDelete(args[0]).then(deletedMessages => {
-if (deletedMessages.size < 1) return message.reply("Hiç mesaj silemedim! _(**14** günden önceki mesajları silemem!)_");
-})
-message.channel.send(`**${args[0]}** adet mesaj başarıyla silindi!`)
+exports.run = async (client, message, args) => {
+  if (!message.member.permissions.has("MANAGE_MESSAGES"))
+    return message.channel.send(
+      new Discord.MessageEmbed().setTitle(
+        "**`Mesajları Yönet` İzni sende yok.**"
+      )
+    );
+  if (!args[0])
+    return message.channel.send(
+      new Discord.MessageEmbed().setTitle(
+        "<:yr_hayr:793837203478020127> Silinecek miktar giriniz."
+      )
+    );
+  if (args[0] > 100)
+    return message.channel.send(
+      new Discord.MessageEmbed().setTitle(
+        "<:yr_hayr:793837203478020127> Mesaj silme limiti `100` üzeri mesajı aynı anda silemem."
+      )
+    );
+  message.channel.bulkDelete(args[0]);
+  return message.channel
+    .send(
+      new Discord.MessageEmbed().setTitle(
+        "<:yr_evet:793837194175447090>  " +
+          `${args[0]}` +
+          " Adet mesaj silindi."
+      )
+    )
+    .then(m => m.delete({ timeout: 5000 }));
 };
-
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-  aliases: ["sil", "mesaj-sil", "mesajları-sil"],
-  permLevel: `Mesajları yönet yetkisine sahip olmak gerekir.`
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'sil',
-  category: 'moderasyon',
-  description: 'Belirtilen miktarda mesaj siler.',
-  usage: '.sil <miktar>'
+  name: "sil"
 };
