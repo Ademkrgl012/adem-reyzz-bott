73576ad61a4b5f7d9135945de60dd9d7652012e1
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 07:55:49 +0000

    🐢🍀 Checkpoint ./bot.js:22384402/23549 ./komutlar/unban.js:22384402/10186

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
-const fs=require('fs');
-const Discord=require("discord.js");
-const client=new Discord.Client();
-const db = require('quick.db')
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const db = require("quick.db");
 const moment = require("moment");
-const ayarlar=require("./ayarlar.json");
-const express = require('express');
+const ayarlar = require("./ayarlar.json");
+const express = require("express");
 /////
-const app = express()
-app.get('/', (req, res) => res.send("Bot Aktif"))
-app.listen(process.env.PORT, () => console.log('Port ayarlandı: ' + process.env.PORT))
+const app = express();
+app.get("/", (req, res) => res.send("Bot Aktif"));
+app.listen(process.env.PORT, () =>
+  console.log("Port ayarlandı: " + process.env.PORT)
+);
 //////////////////
-
-
 
 client.on("message", message => {
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
+  let command = message.content.split(" ")[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(" ").slice(1);
   let perms = client.yetkiler(message);
   let cmd;
   if (client.commands.has(command)) {
@@ -30,24 +30,21 @@ client.on("message", message => {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
-})
-
+});
 
 client.on("ready", () => {
   console.log(`Bütün komutlar başarıyla yüklendi!`);
   client.user.setStatus("online");
   client.user.setActivity("Yrnex Bot Sizlerle | y!yardım y!davet");
-  })                              
-
+});
 
 const log = message => {
   console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
 };
 
-
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
+fs.readdir("./komutlar/", (err, files) => {
   if (err) console.error(err);
   log(`${files.length} adet komut yüklemeye hazırlanılıyor.`);
   files.forEach(f => {
@@ -59,7 +56,6 @@ fs.readdir('./komutlar/', (err, files) => {
     });
   });
 });
-
 
 client.reload = command => {
   return new Promise((resolve, reject) => {
@@ -75,7 +71,7 @@ client.reload = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
@@ -90,7 +86,7 @@ client.load = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
@@ -106,100 +102,111 @@ client.unload = command => {
         if (cmd === command) client.aliases.delete(alias);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
 };
 
-  
 client.yetkiler = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = -ayarlar.varsayilanperm  ;
-  if(message.member.hasPermission("MANAGE_MESSAGES")) permlvl = 1;
-  if(message.member.hasPermission("KICK_MEMBERS")) permlvl = 2;
-  if(message.member.hasPermission("BAN_MEMBERS")) permlvl = 3;
-  if(message.member.hasPermission("MANAGE_GUILD")) permlvl = 4;
-  if(message.member.hasPermission("ADMINISTRATOR")) permlvl = 5;
-  if(message.author.id === message.guild.ownerID) permlvl = 6;
-  if(message.author.id === ayarlar.sahip) permlvl = 7;
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = -ayarlar.varsayilanperm;
+  if (message.member.hasPermission("MANAGE_MESSAGES")) permlvl = 1;
+  if (message.member.hasPermission("KICK_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 3;
+  if (message.member.hasPermission("MANAGE_GUILD")) permlvl = 4;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 5;
+  if (message.author.id === message.guild.ownerID) permlvl = 6;
+  if (message.author.id === ayarlar.sahip) permlvl = 7;
   return permlvl;
 };
 
-
-
 client.on("message", async msg => {
-    if(msg.author.bot) return;
-    
-    let i = await db.fetch(`reklamFiltre_${msg.guild.id}`)  
-          if (i == 'acik') {
-              const reklam = ["https://","http://","discord.gg"];
-              if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
-                try {
-                  if (!msg.member.hasPermission("MANAGE_GUILD")) {
-                    msg.delete();                                       
-                    return msg.channel.send(`${msg.author.tag}, Reklam Yapmak Yasak!`).then(msg => msg.delete(10000));
-                  }              
-                } catch(err) {
-                  console.log(err);
-                }
-              }
-          }
-          if (!i) return;
-          });    
+  if (msg.author.bot) return;
 
-
-client.on("messageUpdate", msg => {
- 
- 
- const i = db.fetch(`${msg.guild.id}.kufur`)
-    if (i) {
-        const kufur = ["oç", 
-                       "amk", 
-                       "amk", 
-                       "ananı sik iyim",
-                       "piç",
-                       "orospu çocuğu",
-                       "orospu",
-                       "oruspu"];
-        if (kufur.some(word => msg.content.includes(word))) {
-          try {
-            if (!msg.member.hasPermission("BAN_MEMBERS")) {
-                  msg.delete();
-                         
-                      return msg.reply('Bu Sunucuda Küfür Filtresi Aktiftir.').then(msg => msg.delete(3000));
-            }              
-          } catch(err) {
-            console.log(err);
-          }
+  let i = await db.fetch(`reklamFiltre_${msg.guild.id}`);
+  if (i == "acik") {
+    const reklam = ["https://", "http://", "discord.gg", ".com"];
+    if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("MANAGE_GUILD")) {
+          msg.delete();
+          return msg.channel
+            .send(`${msg.author.tag}, Reklam Yapmak Yasak!`)
+            .then(msg => msg.delete(10000));
         }
-    }
-    if (!i) return;
-});
- 
-
-
-client.on("message", async msg => {
- 
- 
-  const i = await db.fetch(`ssaass_${msg.guild.id}`);
-    if (i == 'acik') {
-      if (msg.content.toLowerCase() == 'sa' || msg.content.toLowerCase() == 's.a' || msg.content.toLowerCase() == 'selamun aleyküm' || msg.content.toLowerCase() == 'sea'|| msg.content.toLowerCase() == 'selam') {
-          try {
- 
-                  return msg.reply(
-                    '**Aleyküm Selam , Hoşgeldin**')
-          } catch(err) {
-            console.log(err);
-          }
+      } catch (err) {
+        console.log(err);
       }
     }
-    else if (i == 'kapali') {
-   
-    }
-    if (!i) return;
- 
-    });
+  }
+  if (!i) return;
+});
 
-client.login(ayarlar.token)
+client.on("messageUpdate", msg => {
+  const i = db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "amk",
+      "ananı sik iyim",
+      "piç",
+      "orospu çocuğu",
+      "orospu",
+      "oruspu",
+      "aq",
+      "yarrak",
+      "yarra",
+      "anan",
+      "sikim",
+      "sg",
+      "s.g",
+      "amcık",
+      "pic",
+      "piç",
+      "ibine"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.hasPermission("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Bu Sunucuda Küfür Filtresi Aktiftir.")
+            .then(msg => msg.delete(3000));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+client.on("message", async msg => {
+  const i = await db.fetch(`ssaass_${msg.guild.id}`);
+  if (i == "acik") {
+    if (
+      msg.content.toLowerCase() == "sa" ||
+      msg.content.toLowerCase() == "s.a" ||
+      msg.content.toLowerCase() == "selamun aleyküm" ||
+      msg.content.toLowerCase() == "sea" ||
+      msg.content.toLowerCase() == "selam"
+    ) {
+      try {
+        return msg.reply(
+          "**Aleyküm Selam , Hoşgeldin** <a:mutlu:792028555008540684>"
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  } else if (i == "kapali") {
+  }
+  if (!i) return;
+});
+
+client.login(ayarlar.token);
--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,80 +1,90 @@
 const Discord = module.require("discord.js");
- 
+
 module.exports.run = async (client, message, args) => {
- 
   const permError = new Discord.MessageEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 01 •')
-        .setDescription('```Bu Komutu Kullanmak İçin "Üyeleri Yasakla" Yetkisine Sahip Olmalısın```')
-   
+    .setColor("")
+    .setTitle("• Hata: 1 •")
+    .setDescription(
+      '```Bu Komutu Kullanmak İçin "Üyeleri Yasakla" Yetkisine Sahip Olmalısın```'
+    );
+
   const userError = new Discord.MessageEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 02 •')
-        .setDescription('```Yasağı kaldırmak için bir kullanıcı kimliği girmelisiniz g!unban İD```')
- 
+    .setColor("#ed455a")
+    .setTitle("• Hata: 2 •")
+    .setDescription(
+      "```Yasağı kaldırmak için bir kullanıcı kimliği girmelisiniz y!unban İD```"
+    );
+
   const userError2 = new Discord.MessageEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 03 •')
-        .setDescription("```ID'de Harf Kullanılanamaz```")
- 
+    .setColor("#ed455a")
+    .setTitle("• Hata: 3 •")
+    .setDescription("```ID'de Harf Kullanılanamaz```");
+
   const userError3 = new Discord.MessageEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 04 •')
-        .setDescription('```Bu Kullanıcı Yasaklanmamış```')
-   
+    .setColor("#ed455a")
+    .setTitle("• Hata: 4 •")
+    .setDescription("```Bu Kullanıcı Yasaklanmamış```");
+
   const levelError = new Discord.MessageEmbed()
-    .setColor('#ed455a')
-      .setTitle('• Hata: 05 •')
-        .setDescription('```Sizinle aynı veya daha yüksek bir role sahip bir üyenin yasağını kaldırmazsınız```')
- 
- 
-  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send
-        (permError).then
-          (message.delete()).then
-            (msg => msg.delete({timeout: 5000, reason: "xD"}));
- 
+    .setColor("#ed455a")
+    .setTitle("• Hata: 05 •")
+    .setDescription(
+      "```Sizinle aynı veya daha yüksek bir role sahip bir üyenin yasağını kaldırmazsınız```"
+    );
+
+  if (!message.member.hasPermission("BAN_MEMBERS"))
+    return message.channel
+      .send(permError)
+      .then(message.delete())
+      .then(msg => msg.delete({ timeout: 5000, reason: "xD" }));
+
   let user = args[0];
-    if  (!user) return message.channel.send
-          (userError).catch(console.error).then
-            (message.delete()).then
-              (msg => msg.delete({timeout: 5000, reason: "xD"}));
- 
-  if  (isNaN(args[0])) return message.channel.send
-        (userError2).catch(console.error).then
-          (message.delete()).then
-            (msg => msg.delete({timeout: 5000, reason: "xD"}));
- 
-  if  (user.highestRole >= message.author.highestRole) return message.channel.send
-          (levelError).then
-            (message.delete()).then
-              (msg => msg.delete({timeout: 5000, reason: "xD"}));
- 
- 
+  if (!user)
+    return message.channel
+      .send(userError)
+      .catch(console.error)
+      .then(message.delete())
+      .then(msg => msg.delete({ timeout: 5000, reason: "xD" }));
+
+  if (isNaN(args[0]))
+    return message.channel
+      .send(userError2)
+      .catch(console.error)
+      .then(message.delete())
+      .then(msg => msg.delete({ timeout: 5000, reason: "xD" }));
+
+  if (user.highestRole >= message.author.highestRole)
+    return message.channel
+      .send(levelError)
+      .then(message.delete())
+      .then(msg => msg.delete({ timeout: 5000, reason: "xD" }));
+
   const banList = await message.guild.fetchBans();
- 
- // console.log(banList.map(s => s.users.id))
- 
-  if (!user.id === banList) return message.channel.send
-      (userError3).then
-        (message.delete()).then
-          (msg => msg.delete({timeout: 5000, reason: "xD"}));
- 
+
+  // console.log(banList.map(s => s.users.id))
+
+  if (!user.id === banList)
+    return message.channel
+      .send(userError3)
+      .then(message.delete())
+      .then(msg => msg.delete({ timeout: 5000, reason: "xD" }));
+
   message.guild.members.unban(user);
-message.channel.send(`<@!${user}> Adlı Kullanıcının Yasağı Başarıyla Kaldırıldı.`)
- 
-  }
- 
-  exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0,
-        kategori: "Yetkili"
- 
-  };
- 
-  exports.help = {
-    name: 'unban',
-    description: 'İstediğiniz kişinin banını kaldırır.',
-    usage: 'unban [kullanıcı] [sebep]'
-  };
+  message.channel.send(
+    `<@!${user}> Adlı Kullanıcının Yasağı Başarıyla Kaldırıldı.`
+  );
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+  kategori: "Yetkili"
+};
+
+exports.help = {
+  name: "unban",
+  description: "İstediğiniz kişinin banını kaldırır.",
+  usage: "unban [kullanıcı] [sebep]"
+};
