7d4c87ae2723a83da999c7ec6b24aac0bdcae0e8
Glitch (discordjsv12basitaltyapi) <none>
2020-12-31 12:58:44 +0000

    ⚡️💸 Checkpoint ./komutlar/sunucu-kur-nitro.js:22384402/1860

--- komutlar/sunucu-kur-nitro.js
+++ komutlar/sunucu-kur-nitro.js
@@ -1,10 +1,24 @@
 const Discord = require('discord.js');
 const data = require('quick.db');
-
+const talkedRecently = new Set();
 
 exports.run = async (client, message, args) => {
 if(message.author.id !== message.guild.owner.user.id) return message.channel.send(new Discord.MessageEmbed().setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png').setImage('https://cdn.glitch.com/0c8ef551-5187-48a8-9daf-f2cc35630f21%2Fyoneticigif.gif').setTitle('Bir hata oldu!').setDescription(`• \`${client.ayarlar.prefix}sunucu-kur\` **kullanmak için,** \`Sunucu Sahibi\` **olmanız gerekiyor.**`).addField('Sunucu Sahibi', message.guild.owner.user.tag).setImage('https://cdn.glitch.com/6f5bb25b-c11b-4003-8a39-69490341df18%2FScreenshot_1.png'));
 
+    if (talkedRecently.has(message.author.id)) {
+           return message.channel.send("30 Dakikada da Bir Kullanabilirsiniz - " + message.author);
+    } else {
+
+           // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+        message.delete();
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 1800000);// Şuan 30 Dakkadır Değiştirebilirsiniz.
+    }
 message.channel.send(new Discord.MessageEmbed()
 .setTitle('Oyun & Sohbet Tema')
 .setThumbnail('https://cdn.discordapp.com/avatars/686185592899633200/6499d2f1c46b106eed1e25892568aa55.webp?size=512')
