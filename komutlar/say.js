fac7fcb862fa132930c86433a610e2fadc158da7
Glitch (discordjsv12basitaltyapi) <none>
2021-01-07 10:03:35 +0000

    👭🏐 Checkpoint ./komutlar/say.js:22384402/9180 ./komutlar/yapımcım.js:22384402/4408

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,55 +1,82 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-  let boost = message.guild.members.filter(r =>
-    r.roles.has("774982379705466881")
-  ).size;
-  var tagdakiler = 0;
-  let tag = "‡";
-  message.guild.members.forEach(member => {
-    if (member.user.username.includes(tag)) {
-      tagdakiler = tagdakiler + 1;
-    }
-  });
-
-  const voiceChannels = message.guild.channels.filter(c => c.type === "voice");
+exports.run = async (client, message, args) => {
+  let güvenlik = message.guild.verificationLevel;
+  if (güvenlik === "NONE") güvenlik = "Yok";
+  if (güvenlik === "LOW") güvenlik = "Düşük";
+  if (güvenlik === "MEDIUM") güvenlik = "Orta";
+  if (güvenlik === "HIGH") güvenlik = "Yüksek";
+  if (güvenlik === "VERY_HIGH") güvenlik = " En Yüksek";
+  const voiceChannels = message.guild.channels.cache.filter(
+    c => c.type === "voice"
+  );
   let count = 0;
-
+  let guild = message.guild;
+  let botlar = message.guild.members.cache.filter(m => m.user.bot).size;
+  let textChannels = message.guild.channels.cache.filter(m => m.type == "text")
+    .size;
   for (const [id, voiceChannel] of voiceChannels)
     count += voiceChannel.members.size;
-  const emoji = client.emojis.find(emoji => emoji.name === "elmas"); // emojinin adını yazın.
-  const avezyembed = new Discord.RichEmbed()
-    .setThumbnail(client.user.avatarURL)
-    .setColor("black")
-    .addField(
-      `${emoji} **Ses Kanallarında Bulunan Kişi Sayısı** ${emoji}`,
-      `${count} **Kişi Bulunmaktadır.**`
-    ) // emoji yazan yerlere yazdıgınız emoji gelecektir.
-    .addField(
-      `${emoji}  **Sunucuda Bulunan Kişi Sayısı** ${emoji} `,
-      `${message.guild.memberCount} **Kişi Bulunmaktadır.**`
+  let çevrimiçi = message.guild.members.cache.filter(
+    m => !m.user.bot && m.user.presence.status !== "offline"
+  ).size;
+  const embed = new Discord.MessageEmbed()
+    .setColor("BLACK")
+    .setThumbnail(
+      `https://media.discordapp.net/attachments/761099716993613824/771419350828843059/gif_3.gif?width=115&height=115`
     )
+
     .addField(
-      `${emoji} **Taglı Üye Sayısı** ${emoji}`,
-      ` ${tagdakiler}` + ` **Kişi Tagımızda!**`
+      `👥  **__Sunucudaki Toplam Üye Sayısı__**`,
+      `**\`\`\`${message.guild.memberCount}\`\`\`**`
     )
+
+    .addField(`🎧  **__Seslideki Üye Sayısı__**`, `**\`\`\`${count}\`\`\`**`)
+
+    .addField(`🔰  **__Güvenlik Seviyesi__**`, `**\`\`\`${güvenlik}\`\`\`**`)
+    .addField(`💬  **__Yazı Kanalları__**`, `» **${textChannels}**`)
+
+    .addField(`🎶  **__Ses Kanalları__**`, `» **${voiceChannels.size}**`)
+
     .addField(
-      `${emoji} **Sunucu Booster Sayısı** ${emoji}`,
-      ` ${boost}` + ` **Kişi Sunucuyu Boostlamış**`
+      `<a:scroll:771416646547406918>  **__Roller__**`,
+      `»  **${message.guild.roles.cache.size}**`
     )
-    .setFooter(`Created By Avezy`);
-  message.channel.sendEmbed(avezyembed);
-  message.react();
+
+    .addField(
+      `<a:scroll:771416646547406918>  **__Emojiler__**`,
+      `»  **${message.guild.emojis.cache.size}**`
+    )
+
+    .addField(
+      `📊  **__Boost Seviyesi__**`,
+      `»  **${message.guild.premiumTier}/3**`
+    )
+
+    .addField(
+      `🚀  **__Boost Sayısı__**`,
+      `»  **${message.guild.premiumSubscriptionCount}**`
+    )
+
+    .setTitle(
+      `<a:small_orange_diamond:771414619776352277> ${message.author.tag} - Tarafından istendi.`
+    )
+    .setImage(
+      "https://media.discordapp.net/attachments/697145772801785876/716671769355747348/1.gif"
+    )
+    .setFooter(`© 2020 Yrnex Tüm Hakları Saklıdır.`, client.user.avatarURL);
+  message.channel.send(embed);
 };
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["say"],
   permLevel: 0
 };
 
 exports.help = {
   name: "say",
-  description: " Sunucudaki ve sesdeki üyeleri gösterir",
-  usage: "!say"
+  description: "Say",
+  usage: "say"
 };
---
+++ komutlar/yapımcım.js
@@ -0,0 +1,22 @@
+const discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  const wizard = new discord.MessageEmbed()
+    .setColor("RANDOM")
+    .addField(
+      `<a:yr_trnctac:775380721682087936> İşte Benim Yapımcım`,
+      `<@768080464984014918>`,
+      true
+    )
+    .setThumbnail(client.user.avatarURL());
+  message.channel.send(wizard); //narcoscode
+};
+exports.conf = {
+  name: true,
+  guildonly: false,
+  aliases: [],
+  permlevel: 0
+};
+exports.help = {
+  name: "yapımcım"
+};
