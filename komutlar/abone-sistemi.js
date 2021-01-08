8a880b719a8a63d5309a587844a0cada60f0b138
Glitch (discordjsv12basitaltyapi) <none>
2021-01-08 19:43:23 +0000

    💆🏗 Checkpoint ./komutlar/abone-sistemi.js:22384402/110

--- komutlar/abone-sistemi.js
+++ komutlar/abone-sistemi.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
     .setTitle("<:YouTube:791401370560495668> Yrnex Abone Sistemi Komutları! ")
     .setTimestamp()
     .setDescription("<:emoji_1:793928364133056513> **y!abone-yetkili-rol** = Abone Rolünü vericek yetkili rolünü seçer.  \n <:emoji_1:793928364133056513> **y!abone-rol** = Vericeğiniz Rolü ayarlarsınız.  \n <:emoji_1:793928364133056513> **y!abone-log** = Log mesajınn gitceği yer seçilir.  \n <:emoji_1:793928364133056513> **y!abone veya a** = Abone Rolü verme komutudur.")
-    .setImage('https://cdn.discordapp.com/attachments/702883614756700212/722109027794026577/mlogo_1.png')
+    .setImage('https://cdn.discordapp.com/attachments/781872196654071819/797186478261927976/7636_GlowingRGBLine.gif')
   message.channel.send(mod);
 };
 
