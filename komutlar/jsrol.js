f7202aab3f915b77a46a28b4b3a9cdf28b6dd876
Glitch (discordjsv12basitaltyapi) <none>
2021-01-08 15:21:11 +0000

    👕⛳️ Checkpoint ./komutlar/jsrol.js:22384402/79 ./komutlar/kod.js:22384402/2134 ./komutlar/kodpal.js:22384402/8112

--- komutlar/jsrol.js
+++ komutlar/jsrol.js
@@ -1,9 +1,9 @@
 const dc = require("discord.js");
 
 exports.run = async (client, message, args) => {
-  message.member.roles.add("ROL ID")
-  message.react("✅")
-  message.channel.send("✅ • **JavaScript Rolü Başarıyla Verildi.**")
+  message.member.roles.add("797119355912978443")
+  message.react("<:yr_evet:793837194175447090>")
+  message.channel.send("<:js:797120176419897394> • **JavaScript Rolü Başarıyla Verildi.**")
 }
 exports.conf = {
   enabled: true,
