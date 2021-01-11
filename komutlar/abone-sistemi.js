bdbd53f94a93b1cd347cdf3963b3c77edaa4cf50
Glitch (discordjsv12basitaltyapi) <none>
2021-01-11 13:21:05 +0000

    🐒👯 Checkpoint ./komutlar/say.js:22384402/3140 ./komutlar/abone-sistemi.js:22384402/2100

--- komutlar/abone-sistemi.js
+++ komutlar/abone-sistemi.js
@@ -6,8 +6,12 @@ exports.run = async (client, message, args) => {
     .setColor("RANDOM")
     .setTitle("<:YouTube:791401370560495668> Yrnex Abone Sistemi Komutları! ")
     .setTimestamp()
-    .setDescription("<:emoji_1:793928364133056513> **y!abone-yetkili-rol** = Abone Rolünü vericek yetkili rolünü seçer.  \n <:emoji_1:793928364133056513> **y!abone-rol** = Vericeğiniz Rolü ayarlarsınız.  \n <:emoji_1:793928364133056513> **y!abone-log** = Log mesajınn gitceği yer seçilir.  \n <:emoji_1:793928364133056513> **y!abone veya a** = Abone Rolü verme komutudur.")
-    .setImage('https://cdn.discordapp.com/attachments/781872196654071819/797186478261927976/7636_GlowingRGBLine.gif')
+    .setDescription(
+      "<:emoji_1:793928364133056513> **y!abone-yetkili-rol** = Abone Rolünü vericek yetkili rolünü seçer.  \n <:emoji_1:793928364133056513> **y!abone-rol** = Vericeğiniz Rolü ayarlarsınız.  \n <:emoji_1:793928364133056513> **y!abone-log** = Log mesajınn gitceği yer seçilir.  \n <:emoji_1:793928364133056513> **y!abone veya a** = Abone Rolü verme komutudur."
+    )
+    .setImage(
+      "https://cdn.discordapp.com/attachments/781872196654071819/797186478261927976/7636_GlowingRGBLine.gif"
+    );
   message.channel.send(mod);
 };
 
