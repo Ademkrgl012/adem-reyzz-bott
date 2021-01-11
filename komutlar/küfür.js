556a0c38be8490d9285c4526ccc95cb4eb4fecbf
Glitch (discordjsv12basitaltyapi) <none>
2021-01-11 18:08:42 +0000

    ☃️👝 Checkpoint ./komutlar/küfür.js:22384402/25

--- komutlar/küfür.js
+++ komutlar/küfür.js
@@ -4,17 +4,17 @@ const db = require('quick.db')
 exports.run = async (client ,message, args) =>{
   
     if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(` Bu komudu kullanabilmek için "Sunucuyu Yönet" yetkisine sahip olman gerek.`)
-if(args[0] === 'aktif') {
+if(args[0] === 'aç') {
     db.set(`${message.guild.id}.kufur`, true)
     message.channel.send('<:yr_evet:793837194175447090> **Küfür Engel Başarıyla Açıldı.**')
   return
 }
-if (args[0] === 'deaktif') {
+if (args[0] === 'kapat') {
   db.delete(`${message.guild.id}.kufur`)
 message.channel.send('<:yr_hayr:793837203478020127> **Küfür Engel Başarıyla Kapatıldı.**')
 return
 }
-  message.channel.send('**Lüten y!küfür aç` yada `y!küfür` Yazın!**')
+  message.channel.send('**Lüten y!küfür aç` yada `y!küfür kapat` Yazın!**')
 };
 exports.conf = {
  enabled: true,
