0580acbcd83509bb15850f84bf3a7bf66af01f34
Glitch (yrnex-glugin-bot) <none>
2021-01-13 15:10:30 +0000

    🔨🛎 Checkpoint ./komutlar/yardım.js:22384402/148

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,6 +9,7 @@ let yardım = new Discord.MessageEmbed()
 <:emoji_1:793928364133056513> **y!abone-yardım** : Abone Rol Komutları.
 <:emoji_3:793928384932610099> **y!kullanıcı** : Kullanıcı Komutları (YAKINDA).
 <:emoji_4:793928482463154186> **y!eğlence** : Eğlence Komutları (YAKINDA).`)
+.addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot)", )
     .setImage("https://cdn.discordapp.com/attachments/797899158807117867/798281669961842806/standard_22.gif")
 .setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL())
 .setThumbnail(client.user.avatarURL())
