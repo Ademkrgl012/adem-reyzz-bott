faa22e51e3f40aec5fc3c213a27136095bea7c37
Glitch (yrnex-glugin-bot) <none>
2021-01-14 20:50:05 +0000

    🐛🏛 Checkpoint ./komutlar/ailem.js:22384402/789 ./komutlar/davet-kanal.js:22384402/1314 ./bot.js:22384402/1574

--- bot.js
+++ bot.js
@@ -122,7 +122,7 @@ client.elevation = message => {
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };
-client.login(process.env.TOKEN);
+
 client.on("guildMemberRemove", async member => {
   const channel = db.fetch(`sayaçKanal_${member.guild.id}`);
   if (db.has(`sayacsayı_${member.guild.id}`) == false) return;
@@ -785,5 +785,53 @@ client.on('message', msg => {
   }
 })
 
+client.on("guildMemberRemove", async member => {
+  let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
+  if (!kanal) return;
+  let veri = await db.fetch(`rol1_${member.guild.id}`);
+  let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
+  let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
+  let veri2 = await db.fetch(`rol2_${member.guild.id}`);
+  let d = await db.fetch(`bunudavet_${member.id}`);
+  const sa = client.users.get(d);
+  const sasad = member.guild.members.get(d);
+  let sayı2 = await db.fetch(`davet_${d}_${member.guild.id}`);
+  db.add(`davet_${d}_${member.guild.id}`, -1);
+
+  if (!d) {
+    const aa = new Discord.RichEmbed()
+      .setColor("BLACK")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **Left :outbox_tray: \nInvited By:** \`\`Bulunamadı\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL);
+    client.channels.get(kanal).send(aa);
+    return;
+  } else {
+    const aa = new Discord.RichEmbed()
+      .setColor("BLACK")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **Left :outbox_tray: \nInvited By:** \`\`${sa.tag}\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL);
+    client.channels.get(kanal).send(aa);
+
+    if (!veri) return;
+
+    if (sasad.roles.has(veri)) {
+      if (sayı2 <= veri12) {
+        sasad.removeRole(veri);
+        return;
+      }
+    }
+    if (sasad.roles.has(veri2)) {
+      if (!veri2) return;
+      if (sayı2 <= veri21) {
+        sasad.removeRole(veri2);
+        return;
+      }
+    }
+  }
+});
 
 client.login(ayarlar.token);
