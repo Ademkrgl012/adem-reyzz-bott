7f971021d6e4691de8066d6440a66f78fc7b2993
Glitch (yrnex-glugin-bot) <none>
2021-01-17 17:39:42 +0000

    🏄☕️ Checkpoint ./komutlar/otorol.js:22384402/22771

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,44 +1,95 @@
 const Discord = require("discord.js");
-
-const client = new Discord.Client();
-
 const db = require("quick.db");
 
 exports.run = async (client, message, args) => {
   if (!message.member.hasPermission("MANAGE_ROLES"))
     return message.channel.send(
-      "**Hop dayı nereye yetkin yok kullanamazsın.**"
+      "**Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin** `Rolleri Yönet`"
     );
+
   if (!args[0])
     return message.channel.send(
-      "Doğru kullanım ``y!otorol ayarla @rol #kanal`` veya `y!otorol sıfırla`"
+      new Discord.MessageEmbed()
+        .setColor("0x36393E")
+        .setDescription(
+          "> **Otorol sistemini ne yapmak istediğini belirtmedin** <a:yrnex_tiks:798275047047168041> \n> `y!otorol ayarla @üye #kanal` **Otorol sistemini ayarlar.** \n> `y!otorol sıfırla` **Otorol sistemini sıfırlar.**"
+        )
     );
+
   if (args[0] === "ayarla") {
     var rol = message.mentions.roles.first();
-    var kanal = message.mentions.channels.first();
-    if (!rol) return message.channel.send("Bİr rol etiketlemelisin!!!");
-    if (!kanal) return message.channel.send("Bir kanal etiketlemelisin!!");
-    db.set(`otorol_${message.guild.id}`, rol.id);
-    db.set(`logkanal_${message.guild.id}`, kanal.id);
-    message.channel.send("Başarıyla ayarlandı");
-    
+    var rolkanal = message.mentions.channels.first();
+    if (!rol)
+      return message.channel.send(
+        new Discord.MessageEmbed()
+          .setColor("0x36393E")
+          .setDescription(
+            "**Bir rol etiketlemelisin** `y!otorol ayarla @rol #kanal` **(Eğer rolü bulamıyorsan etiketleme izninin açık olduğundan veya komutun kullanıldığı kanalı görebildiğinden emin ol)**"
+          )
+      );
+    if (!rolkanal)
+      return message.channel.send(
+        new Discord.MessageEmbed()
+          .setColor("0x36393E")
+          .setDescription(
+            "**Bir kanal etiketlemelisin eğer kanalı etiketleyemiyorsan botun o kanalı gördüğünden emin ol.**"
+          )
+      );
 
-    if (args[0] === "sıfırla") {
-      db.delete(`otorol_${message.guild.id}`);
-      db.delete(`logkanal_${message.guild.id}`);
-      message.channel.send("**Başarıyla sıfırla.**");
-    }
+    db.set(`logkanal_{message.guild.id}`, rolkanal.id);
+    db.set(`otorol_${message.guild.id}`, rol.id);
+
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor("0x36393E")
+        .setDescription(
+          "> **Otorol Sistemi Aktif edildi** <a:yrnex_tiks:798275047047168041> \n> **Sunucuya giren kişilere verilecek rol** <@&" +
+            rol +
+            "> \n> **Otorol mesajının gideceği kanal** <#" +
+            rolkanal.id +
+            "> **Olarak ayarlandı.**"
+        )
+    );
+  }
+
+  if (args[0] === "sıfırla") {
+    let user = message.author;
+    message.channel
+      .send(
+        "**Otorol Sistemini sıfırlamak istediğinden eminmisin eğer eminsen** :white_check_mark: **değilsen** :x: **Tepkisine bas.**"
+      )
+      .then(async m => {
+        await m.react("✅").then(r => {
+          let onay = (reaction, user) =>
+            reaction.emoji.name === "✅" && user.id == message.author.id;
+          let onay2 = m.createReactionCollector(onay);
+          onay2.on("collect", async r => {
+            db.delete(`logkanal_{message.guild.id}`);
+            db.delete(`otorol_${message.guild.id}`);
+            m.reactions.removeAll();
+            m.edit("**Otorol sistemi kapatıldı.** <a:yrnex_tiks:798275047047168041>");
+          });
+        });
+        await m.react("❌").then(r => {
+          let onay = (reaction, user) =>
+            reaction.emoji.name == "❌" && user.id == message.author.id;
+          let onay2 = m.createReactionCollector(onay);
+          onay2.on("collect", async r => {
+            m.reactions.removeAll();
+            m.edit("**Otorol kapatma işlemi iptal edildi.** <a:yrnex_tiks:798275047047168041>");
+          });
+        });
+      });
   }
 };
 
 exports.conf = {
   enabled: true,
+  guildOnly: false,
   aliases: [],
-  permLevel: 2
+  permlevel: 0
 };
 
 exports.help = {
-  name: "otorol",
-  description: "Otorol sistemi",
-  usage: "bla bla bla bla"
+  name: "otorol"
 };
