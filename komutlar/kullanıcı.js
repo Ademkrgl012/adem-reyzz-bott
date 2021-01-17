e647e79a73cc50cf857ceb9547e2af1272a19b54
Glitch (yrnex-glugin-bot) <none>
2021-01-17 16:53:26 +0000

    🐷🌮 Checkpoint ./komutlar/profil.js:22384402/3321 ./komutlar/kullanıcı.js:22384402/2697 ./komutlar/moderasyon.js:22384402/267

---
+++ komutlar/kullanıcı.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => { 
+let prefix = 'y!'
+let yardım = new Discord.MessageEmbed()  
+.setAuthor(`${client.user.username}`, client.user.avatarURL())
+.setColor('RANDOM')
+.addField('<:coffeee:794115205176885298> Yrnex Kullanıcı Menüsü',`
+<:emoji_3:793928384932610099> **y!profil** : Kullanıcı Bilginizi Görürsünüz.
+<:emoji_3:793928384932610099> **y!avatar** : Avatarınıza Bakarsınız.
+<:emoji_3:793928384932610099> **y!yetkilerim** : Sunucudaki Yetkinize Bakarsınız.
+<:emoji_3:793928384932610099> **y!ping** : Botun Pingine Bakarsınız.
+<:emoji_3:793928384932610099> **y!i** : Botun İstatistiklerine Bakarsınız.
+<:emoji_3:793928384932610099> **y!bug-bildir** : Botun Bugunu Bildirirsiniz.
+<:emoji_3:793928384932610099> **y!davet** : Botu Davet Edersiniz.`)
+.addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot)", )
+    .setImage("https://cdn.discordapp.com/attachments/797899158807117867/798281669961842806/standard_22.gif")
+.setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL())
+.setThumbnail(client.user.avatarURL())
+ message.channel.send(yardım) 
+  };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['kullanıcı'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "kullanıcı",
+  category: "kullanıcı",
+    description: "Eğlence Komutları Gösterir."
+};
--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -2,11 +2,8 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
   let prefix = "y!";
   let yardım = new Discord.MessageEmbed()
-    .setAuthor(`${client.user.username}`, client.user.avatarURL())
     .setColor("RANDOM")
-    .addField(
-      "<:yrnex_pols:794269931432181830> Moderasyon Menüsü",
-      `
+    .addField("<:yrnex_pols:794269931432181830> Moderasyon Menüsü",`
 <:emoji_1:793928339261227020> **y!reklam** : Reklam Engeli Açarsınız.
 <:emoji_1:793928339261227020> **y!küfür** : Küfür Engeli Açarsınız.
 <:emoji_1:793928339261227020> **y!otorol** : Otorol Ayarlarsınız.
@@ -16,15 +13,10 @@ exports.run = async (client, message, args) => {
 <:emoji_1:793928339261227020> **y!yavaş-mod** : Yavaş Mod Ayarlarsınız.
 <:emoji_1:793928339261227020> **y!sa-as** : Sa-As Mesajını Açar..
 <:emoji_1:793928339261227020> **y!sil** : Belli Miktarda Mesaj Siler.
-<:emoji_1:793928339261227020> **y!oylama** : Oylama Yaparsınız.`
-    )
-    .setImage(
-      "https://cdn.discordapp.com/attachments/797899158807117867/798281669961842806/standard_22.gif"
-    )
-    .setFooter(
-      `${message.author.tag} Tarafından İstendi.`,
-      message.author.avatarURL()
-    )
+<:emoji_1:793928339261227020> **y!oylama** : Oylama Yaparsınız.`)
+  .addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=787328444954050570&permissions=8&scope=bot)", )
+    .setImage("https://cdn.discordapp.com/attachments/797899158807117867/798281669961842806/standard_22.gif")
+    .setFooter(`${message.author.tag} Tarafından İstendi.`,message.author.avatarURL())
     .setThumbnail(client.user.avatarURL());
   message.channel.send(yardım);
 };
