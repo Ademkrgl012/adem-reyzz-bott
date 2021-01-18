80c053c3a87a2c1a03dd0303522c66dc40a0c823
Glitch (yrnex-glugin-bot) <none>
2021-01-18 15:25:44 +0000

    🌾🏉 Checkpoint ./komutlar/istatistik.js:22384402/8

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -17,7 +17,7 @@ let motion = new Discord.MessageEmbed()
     ) 
     .addField(
       "Bot Geliştiricisi",
-      `> <a:yrnex_cevelop:800735697060888646> Bot geliştiricisi : <@768080464984014918> | **Yrnex Creative' S#0612**`
+      `> <a:yrnex_cevelop:800735697060888646> Bot geliştiricisi : <@768080464984014918> | **Yrnex Creative' S#9999**`
     ) 
     .addField(
       "Sürümler",
