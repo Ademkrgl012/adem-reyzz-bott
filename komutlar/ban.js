1a51bec972c44fa68e05a32f6aba39696d24bb65
Glitch (yrnex-glugin-bot) <none>
2021-01-20 07:59:16 +0000

    🚉👨‍❤️‍💋‍👨 Checkpoint ./komutlar/ban.js:22384402/5571 ./komutlar/kick.js:22384402/4183

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,16 +1,15 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
-  if (!message.member.hasPermission("KICK_MEMBERS"))
-    return message.channel.send(`:x: Yeterli yetkiye sahip değilsin.`);
 
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`:x: Yeterli yetkiye sahip değilsin.`)
+
+ 
   let member = message.mentions.users.first();
-  if (!member) return message.channel.send(`:x: Lütfen Bir Kişi belirt!`);
-
+  if (!member) return message.channel.send(`:x: Lütfen Bir Kişi belirt!`)
+ 
   const embed = new Discord.MessageEmbed()
     .setColor("BLACK")
-    .setDescription(
-      `${member} Adlı kişinin sunucudan atılmasını onaylıyor musunuz?`
-    );
+    .setDescription(`${member} Adlı kişinin sunucudan banlamasını onaylıyor musunuz?`)
   message.channel.send(embed).then(async function(sentEmbed) {
     const emojiArray = ["✅"];
     const filter = (reaction, user) =>
@@ -19,18 +18,16 @@ exports.run = async (client, message, args) => {
     var reactions = sentEmbed.createReactionCollector(filter, {
       time: 30000
     });
-    reactions.on("end", () => sentEmbed.edit("Kick İşlemi iptal oldu!"));
+    reactions.on("end", () => sentEmbed.edit("Ban İşlemi iptal oldu!"));
     reactions.on("collect", async function(reaction) {
       if (reaction.emoji.name === "✅") {
-        message.channel.send(
-          `İşlem Başarılı! ${member} Adlı Kişi başarılı bir şekilde sunucudan atıldı!`
-        );
-        message.guild.member(member).kick();
+        message.channel.send(`İşlem Başarılı! ${member} Adlı Kişi başarılı bir şekilde sunucudan banlandı!`)
+        message.guild.member(member).ban();
       }
     });
   });
 };
-
+ 
 exports.conf = {
   aliases: [],
   permLevel: 0,
@@ -38,9 +35,9 @@ exports.conf = {
   guildOnly: true,
   kategori: "moderasyon"
 };
-
+ 
 exports.help = {
-  name: "kick",
-  description: "",
-  usage: ""
+  name: "ban",
+  description: "ban",
+  usage: "ban"
 };
