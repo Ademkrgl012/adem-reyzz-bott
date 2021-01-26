01600f0c25cb574eedf4d3a03f009e22119f34f0
Glitch (yrnex-glugin-bot) <none>
2021-01-26 13:37:50 +0000

    💿🚁 Checkpoint ./komutlar/vine.js:22384402/15223 ./komutlar/yapımıcıyı-çağır.js:22384402/1246 ./komutlar/taç-kimde.js:22384402/1635

--- komutlar/taç-kimde.js
+++ komutlar/taç-kimde.js
@@ -1,30 +1,38 @@
-  const Discord = require("discord.js");
+const Discord = require("discord.js");
 const ayarlar = require("../ayarlar.json");
 
 exports.run = async (bot, message, args) => {
-message.delete();
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.MessageEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('**Komutları Özel Mesajlarda Kullanılamaz!**')
+    return message.author.send(ozelmesajuyari); }
+  
+  message.delete();
 
-const pinkcode = new Discord.MessageEmbed()
+const CrewCode = new Discord.MessageEmbed()
 
-    .setColor("BLACK")
+    .setColor("YELLOW")
     .setDescription(
-      `Sunucunun tacı şuanda , kurucu olan bu Güzel Kardeşimde.<@${message.guild.owner.id}>`
+      `**Sunucunun kurucusu bu havalı kişi ;** <@${message.guild.owner.id}>`
     )
     .setFooter(`Komutu kullanan: ${message.author.tag}`);
 
-return message.channel.send(pinkcode).then(msg => {
+return message.channel.send(CrewCode).then(msg => {
 msg.delete(30000);
   });
 };
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['kurucu','taç','sahip'],
+  aliases: ['kurucu','tac','sahip'],
   permLevel: 0
 };
 
 exports.help = {
-  name: "taç-kimde",
+  name: "taç",
   description: "",
   usage: ""
 };
