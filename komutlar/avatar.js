30b34e7a47a1bf49280849e26b0b83d93741f605
Glitch (yrnex-glugin-bot) <none>
2021-01-31 14:24:08 +0000

    🛳🌇 Checkpoint ./komutlar/avatar.js:22384402/11657

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -1,27 +1,25 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message, args) => {
-  let avatar = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=2048`//avatar komutu nasıl bulamadım bende böyle bişey yaptım v12 için yanlız bu :d
-let Embed = new MessageEmbed()
+  let avatar = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=2048`; //avatar komutu nasıl bulamadım bende böyle bişey yaptım v12 için yanlız bu :d
+  let Embed = new MessageEmbed();
 
-            Embed.setTitle(`Avatarın!`)
-            Embed.setThumbnail(avatar)
-            Embed.setColor(`RANDOM`)
-            Embed.setDescription(`**İşte Avatar'ın**`)
-            return message.channel.send(Embed)
-        
-    }
-
+  Embed.setTitle(`Senin avatarın!`);
+  Embed.setThumbnail(avatar);
+  Embed.setColor(`RANDOM`);
+  Embed.setDescription(`Arrow Bot`);
+  return message.channel.send(Embed);
+};
 
 exports.conf = {
-    enabled:false,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0,
-}
+  enabled: false,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
 
 exports.help = {
-    name: 'avatar',
-    description: 'avatarınızı mesaj olarak atar',
-    usage: '(prefix)avatar '
-}
+  name: "avatar",
+  description: "",
+  usage: ""
+};
