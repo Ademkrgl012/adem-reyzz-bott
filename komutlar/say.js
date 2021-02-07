a03ecc29426ca6403fbe602f7a6def22f675e5e3
Glitch (yrnex-glugin-bot) <none>
2021-02-07 20:27:22 +0000

    🍓☂️ Checkpoint ./komutlar/say.js:22384402/787

--- komutlar/say.js
+++ komutlar/say.js
@@ -8,9 +8,10 @@ exports.run = async (client, message, args) => {
         .setColor(message.guild.me.displayColor)
     .setTitle(message.guild.name)
         .addField("<a:yrnex_mor:798254164776976425> Sunucudaki üye sayısı", message.guild.memberCount)
-        .addField("<a:yrnex_discord:794118212693917718> Sunucudaki Bot Sayısı", message.guild.members.cache.filter(m => m.user.bot).size)
         .addField("<a:yrnex_online:798261474489401354> Çevrimiçi üye sayısı", message.guild.members.cache.filter(m => m.user.presence.status !== "offline").size)
         .addField("<a:yrnex_offline:798261454486634544> Çevrimdışı üye sayısı", message.guild.members.cache.filter(m => m.user.presence.status == "offline").size)
+        .addField("<a:yrnex_discord:794118212693917718> Sunucudaki Bot Sayısı", message.guild.members.cache.filter(m => m.user.bot).size)
+        .addField(`<a:warning:771417064946663445>  **__Boost Sayısı__**`,`»  **${message.guild.premiumSubscriptionCount}**`)
 
     message.channel.send(say);
 
