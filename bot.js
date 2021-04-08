e5aba9d4a68d736206c9b5687b30cfff0620645f
Glitch (bidaha-ellemicem-) <none>
2021-04-08 15:07:44 +0000

    Rewound to commit cbd0eae93f600e73ce1c45c0c0777f8f26f21540 + 0 edits

--- bot.js
+++ bot.js
@@ -1037,28 +1037,6 @@ function exp(message) {
     }
 }
 });
-//////////////////////////////giriş-çıkış
-const channel = member.guild.channels.find('name', 'gelen-giden');// 'gelen-giden' log ismidir. değiştirebilirsiniz. belirttiğiniz isme giriş çıkış gösterecektir.if (!channel) return;
-  const embed = new Discord.RichEmbed()
-  .setColor('0x00cc44')
-  .setAuthor(bot.user.username, bot.user.avatarURL)
-  .setThumbnail(member.user.avatarURL)
-  .setTitle(`:inbox_tray: ${member.user.username} Sunucuya katıldı.`)
-  .setTimestamp()
-  channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberRemove', member => {
-  const channel = member.guild.channels.find('name', 'gelen-giden');// 'gelen-giden' log ismidir. değiştirebilirsiniz. belirttiğiniz isme giriş çıkış gösterecektir.
-  if (!channel) return;
-  const embed = new Discord.RichEmbed()
-  .setColor('0xff1a1a')
-  .setAuthor(bot.user.username, bot.user.avatarURL)
-  .setThumbnail(member.user.avatarURL)
-  .setTitle(`:outbox_tray: ${member.user.username} Sunucudan ayrıldı.`)
-  .setTimestamp()
-  channel.sendEmbed(embed);
-});
 //////////////////////////////
 client.login(process.env.Token).then(a => {
   console.log(`✅ Tokene Bağlanıldı | Bot "${client.user.tag}" İsmi İle Giriş Yaptı. || Zego Share`)}).catch(a => {
