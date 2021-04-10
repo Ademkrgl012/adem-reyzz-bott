679962d8602d40d2bfefaea608ffd37c32bc77ae
Glitch (bidaha-ellemicem) <none>
2021-04-10 10:39:38 +0000

    Rewound to commit 4e7378753e718c53ee336381b0768affd549340f + 0 edits

--- bot.js
+++ bot.js
@@ -1218,26 +1218,6 @@ client.on("guildMemberAdd", async member => {
   if (member.user.bot)
     return canvaskanal.send(`🤖 Bu bir bot, ${member.user.tag}`);
 });
-//////////////////////////////Resimli Hg gg
-client.on('guildMemberAdd', async member => {
-require("moment-duration-format");
-    moment.locale('tr')
-    let user = client.users.get(member.id);
-    let tarih = moment(member.user.createdAt.getTime()).format('LLL')
-    let gün = moment.duration(new Date().getTime() - member.user.createdAt.getTime()).format("D")
-    let resim = new Discord.Attachment('https://cdn.discordapp.com/attachments/713874856143355935/714443923338297364/giphy.gif')
-    let kişi = member.guild.memberCount
-    let kayıtcırol = "813875678100455425" //Yetkili rolünüz ID'sini girin.
-    let kanal = client.channels.get("830023039160025128") //Kanalınızın ID'sini girin.
-    const kurulus = new Date().getTime() - user.createdAt.getTime();
-    const gün1 = moment.duration(kurulus).format("D")   
-    var devtr;
-    if (kurulus < 15) devtr = 'Güvenilir Değil!'
-    if (kurulus > 15) devtr = 'Güvenilir!'
-
-    kanal.send(`Merhaba <@${member.user.id}> hanedanımıza **hoşgeldin!**\n\nSeninle beraber **${kişi}** kişiyiz.\n\nTagımızı alarak bize destek olabilirsin\n\nHesap kuruluş tarihi; **${tarih}** [**${gün}** gün önce]\n\n${kayıtcırol} sizinle ilgilenecektir. \n \n Hesabı ${devtr} `, resim)
-})
-
 //////////////////////////////
 client.login(process.env.Token).then(a => {
   console.log(`✅ Tokene Bağlanıldı | Bot "${client.user.tag}" İsmi İle Giriş Yaptı. || Zego Share`)}).catch(a => {
