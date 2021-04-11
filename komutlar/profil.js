815852df1aa4cfec2e6ee8d8b3bfd88b33f73342
Glitch (bidaha-ellemicem) <none>
2021-04-11 18:52:53 +0000

    💫🍡 Checkpoint ./komutlar/profil.js:28514529/6659

--- komutlar/profil.js
+++ komutlar/profil.js
@@ -26,11 +26,11 @@ exports.run = function (client, message, args) {
   
   var Durum = message.author.presence.status;
         var Durm = (Durum == "online" ? (0x00AE86) : (Durum == "offline" ? (0x808080) : (Durum == "idle" ? (0xFFFF00) : (Durum == "dnd" ? (0xFF0000) : (0x00AE86)))))
-        var durm = (Durum == "online" ? ("Çevrimiçi ") : (Durum == "offline" ? ("Çevrimdışı ") : (Durum == "idle" ? ("Boşta ") : (Durum == "dnd" ? ("Rahatsız Etmeyin ") : ("Bilinmiyor/bulunamadı.")))))
+        var durm = (Durum == "online" ? ("Çevrimiçi :online:") : (Durum == "offline" ? ("Çevrimdışı :of:") : (Durum == "idle" ? ("Boşta :ay:") : (Durum == "dnd" ? ("Rahatsız Etmeyin :dnd:") : ("Bilinmiyor/bulunamadı.")))))
   
   var üye = message.mentions.users.first();
   if (üye) {
-    const embed = new Discord.MessageEmbed()
+    const embed = new Discord.RichEmbed()
 .setAuthor(üye.username, üye.displayAvatarURL)
 .setColor('GREEN')
 .setThumbnail(üye.displayAvatarURL)
@@ -39,13 +39,13 @@ exports.run = function (client, message, args) {
         .setFooter(`${client.user.tag} | ${prefix}yardım`)
 message.channel.send(embed)
   } else {
-const embed = new Discord.MessageEmbed()
+const embed = new Discord.RichEmbed()
 .setAuthor(message.author.username, message.author.avatarURL)
 .setColor('GREEN')
-.setThumbnail(message.author.avatarURL())
-.addField('Profil', `**Ad:** ${message.author.username + '#' + message.author.discriminator}\n**ID: ** ${message.author.id}\n**Son Mesaj: ** ${message.author.lastMessage}\n**Son Mesaj İD: ** ${message.author.lastMessageID}\n**Oynadığı Oyun: ** ${message.author.presence.game ? message.author.presence.game.name : 'Şu an oyun oynamıyor'}\n**Durum** ${durm}\n**Oluşturulduğu Tarih: ** ${(`${moment(message.author.createdAt).format('DD')} ${aylar[moment(message.author.createdAt).format('MM')]} ${moment(message.author.createdAt).format('YYYY HH:mm:ss')}`)}\n**Bot mu?** ${message.author.bot ? ':white_check_mark:' : ':negative_squared_cross_mark:'}\n**Roller: ** ${message.guild.members.cache.get(message.author.id).roles.cache.filter(r => r.name !== "@everyone").map(r => r).join(' **|** ')}`)
+.setThumbnail(message.author.avatarURL)
+.addField('Profil', `**Ad:** ${message.author.username + '#' + message.author.discriminator}\n**ID: ** ${message.author.id}\n**Son Mesaj: ** ${message.author.lastMessage}\n**Son Mesaj İD: ** ${message.author.lastMessageID}\n**Oynadığı Oyun: ** ${message.author.presence.game ? message.author.presence.game.name : 'Şu an oyun oynamıyor'}\n**Durum** ${durm}\n**Oluşturulduğu Tarih: ** ${(`${moment(message.author.createdAt).format('DD')} ${aylar[moment(message.author.createdAt).format('MM')]} ${moment(message.author.createdAt).format('YYYY HH:mm:ss')}`)}\n**Bot mu?** ${message.author.bot ? ':white_check_mark:' : ':negative_squared_cross_mark:'}\n**Roller: ** ${message.guild.members.get(message.author.id).roles.filter(r => r.name !== "@everyone").map(r => r).join(' **|** ')}`)
 .setTimestamp()
-.setFooter(`${client.user.tag} | ${prefix}yardım`)
+        .setFooter(`${client.user.tag} | DevTR`)
 message.channel.send(embed)
   
   }
@@ -59,6 +59,4 @@ exports.conf = {
 };
 exports.help = {
   name: 'profil',
-  description: 'Profil.',
-  usage: 'profil'
 };
