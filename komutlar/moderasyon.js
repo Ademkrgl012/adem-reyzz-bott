e4d86caf284de7db7b86d14a488f4add6e8b2fa9
Glitch (bidaha-ellemicem) <none>
2021-04-17 16:01:19 +0000

    ⛹⛹ Checkpoint ./komutlar/özel-url.js:28514529/1637 ./komutlar/moderasyon.js:28514529/63

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -22,7 +22,8 @@ exports.run = async (client, message, args) => {
 🔰 **a!komutlar**: Bottaki Komut Sayısını Gösterir.
 🔰 **a!otocevap**: Yazdığınız Cümleyi Cevaplamayı Açarsanız O Cümleye Cevap Verir.
 🔰 **a!mesajat**: Etiketlediğiniz Kişiye Mesaj Atar.
-🔰 **a!panel**: Sunucu panelini gösterir.`)
+🔰 **a!panel**: Sunucu panelini gösterir.
+🔰 **a!özel-url**: Sunucunuza Özel Link Oluşturur.`)
   .addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=826508857463275542&scope=bot&permissions=2147483647)", )
     .setImage("https://cdn.discordapp.com/attachments/813881349004984370/827103972581048340/standard_1.gif")
     .setFooter(`${message.author.tag} Tarafından İstendi.`,message.author.avatarURL())
